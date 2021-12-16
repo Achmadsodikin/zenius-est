0a051f17bb922beab8b65b26a1f23bb9e24c90f4
asodikin <achmadsodikin.xrpl2.16@gmail.com>
2021-12-16 11:32:50 +0700

    perubahan tambahan jawaban di no.5

--- paket-9aeff790206b11eca44ead25bf23b707/d809b5e2206a11eca44ead25bf23b707/jawaban.js
+++ paket-9aeff790206b11eca44ead25bf23b707/d809b5e2206a11eca44ead25bf23b707/jawaban.js
@@ -9,3 +9,16 @@ for(var i = 1; i <= 2147; i++){
    bilanganPrima += f % 1234567916544
 }
 console.log("hasil sigma a(n) untuk 1147<=n<=2147 adalah :",bilanganPrima);
+
+
+// var bilanganPrima = 0
+// for(var i = 1; i <= 2147; i++){
+//     var f = (i-1) + (i-2)
+//     if ( f <1147  ){
+//         continue;
+//     }else if(f >2147){
+//         break;
+//     }
+//    bilanganPrima += f % 1234567916544
+// }
+// console.log("hasil sigma a(n) untuk 1147<=n<=2147 adalah :",bilanganPrima);
